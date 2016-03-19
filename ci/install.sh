4dcd222245c9d63465171467fb416f4fa624dcf4
Jorge Aparicio <japaricious@gmail.com>
2016-03-19 18:22:39 -0500

    travis: also cross compile to mipsel-musl

--- ci/install.sh
+++ ci/install.sh
@@ -2,13 +2,40 @@
 
 set -ex
 
-sudo apt-get install -qq gcc-arm-linux-gnueabihf
+install_multirust() {
+  git clone https://github.com/brson/multirust
 
-# Install multirust
-git clone https://github.com/brson/multirust
-pushd multirust
-./build.sh
-./install.sh --prefix=~/multirust
-popd
+  pushd multirust
+  ./build.sh
+  ./install.sh --prefix=~/multirust
 
-multirust default $RUST_CHANNEL
+  multirust default nightly
+
+  rustc -V
+  cargo -V
+
+  popd
+}
+
+install_openwrt_sdk() {
+  local root_url="https://downloads.openwrt.org/snapshots/trunk/malta/generic/"
+  local tarball=$(curl $root_url  | grep SDK | cut -d'"' -f2)
+  local filename="${tarball%.*}"
+  filename="${filename%.*}"
+
+  mkdir $STAGING_DIR
+  curl -sL "${root_url}/${tarball}" | \
+    tar --strip-components 2 -C $STAGING_DIR -xj "${filename}/staging_dir"
+
+  PATH="$PATH:$(find $STAGING_DIR -maxdepth 1 -name 'toolchain*' -print -quit)/bin" \
+    mipsel-openwrt-linux-gcc -v
+}
+
+main() {
+  install_multirust
+  if [ "$TARGET" = "mipsel-unknown-linux-musl" ]; then
+    install_openwrt_sdk
+  fi
+}
+
+main
